d8c5cda9a9610e62c2828782b22f9d9a7a8a69e7
kmavrommatis <k_mavromatis@yahoo.gr>
2015-06-28 03:55:25 +0000

    Create bigwig files from bedgraph (produced by MACS2) Updated Mimetypes to recognize the output from MACS2

---
+++ scripts_for_LSF/bin/56.bsub.buildWig.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# this script is using macs2 to call peaks from chipseq datasets
+#
+# it can be used to either call broad or sharp peaks
+# it will ask the db for the type of ChIPseq experiment and adjust accordingly
+
+
+
+scriptDir=$( dirname $0 ); source $scriptDir/../lib/shared.sh
+inputBedGraph=$1
+checkfile $inputBedGraph
+chromInfo=$(dirname $inputBedGraph)/chromInfo.txt
+stem=$(fileStem $inputBedGrapha)
+
+step="peak-wig"
+
+analysistask=38
+
+ 
+export NGS_LOG_DIR=${NGS_LOG_DIR}/${step}
+mkdir -p $NGS_LOG_DIR
+cores=2 #MACS does not need many cores, but when run many instances on the same machine there are crashes.
+memory=3000
+header=$(bsubHeader $stem $step $memory $cores)
+
+
+
+echo \
+"$header
+
+#$Date: 2015-06-01 18:02:35 -0700 (Mon, 01 Jun 2015) $ $Revision: 1524 $
+
+source $scriptDir/../lib/shared.sh 
+set -e
+initiateJob $stem $step
+
+
+
+
+
+inputBedGraph=\$( stage.pl --operation out --type file  ${inputBedGraph} )
+
+####################
+if [ \$inputBedGraph == \"FAILED\"  ] ; then
+	echo \"Could not transfer \$inputBedGraph  \"
+	exit 1
+fi
+
+outputDirectory=\$( setOutput \$inputTag ${step} )
+
+
+celgeneExec.pl --analysistask ${analysistask} \"\
+$bedtoolsbin slop -i \${inputBedGraph} -g \${chromInfo} -b 0 | \
+$bedClipbin stdin /\${chromInfo} stdout | \
+$bedtoolsbin sort -i - > \${outputDirectory}/${stem}/${stem}.bdg.clip ; \
+$bedGraphToBigWigbin \${outputDirectory}/${stem}/${stem}.bdg.clip \${chromInfo} \${outputDirectory}/${stem}/${stem}_treat_pileup.bigwig ; \
+\"
+
+
+ingestDirectory \$outputDirectory yes
+if [ \$? -ne 0 ] ; then
+	echo \"Failed to ingest data\"
+	exit 1
+fi 
+
+
+closeJob
+"> ${stem}.${step}.bsub
+
+bsub < ${stem}.${step}.bsub
