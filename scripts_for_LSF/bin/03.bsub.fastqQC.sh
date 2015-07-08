3126e15ac0d8cf56ae178a355fd4b022b78930d4
kmavrommatis <k_mavromatis@yahoo.gr>
2015-07-07 18:08:44 -0700

    Fixed typo

--- scripts_for_LSF/bin/03.bsub.fastqQC.sh
+++ scripts_for_LSF/bin/03.bsub.fastqQC.sh
@@ -41,7 +41,7 @@ $fastqcbin \
 mv \${outputDirectory}/tmp \${output} \" 
 
 cd \${output}
-for z in *.zip; do unzip -o $z; done
+for z in *.zip; do unzip -o \$z; done
 
 runQC-fastq.pl --logfile \$MASTER_LOGFILE --inputfq \${input},\${input2} --outputfile \${output} --reuse --qcStep FastQC
 if [ \$? != 0 ] ; then
