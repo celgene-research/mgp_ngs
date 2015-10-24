3d307aca794224a4474f48f04eb6aee0d3f11ef8
kmavrommatis <kmavrommatis@celgene.com>
2015-10-23 17:50:36 -0700

    Modified SplitNCigar tool according to new guidelines by Broad (https://www.broadinstitute.org/gatk/blog?id=4285)

--- scripts_for_LSF/bin/50.bsub.splitNcigar.sh
+++ scripts_for_LSF/bin/50.bsub.splitNcigar.sh
@@ -53,8 +53,8 @@ celgeneExec.pl --analysistask $analysistask \"java -Xmx${memory}m -jar ${gatkbin
   $fixQual \
   -I \${input} \
   -o \${outputDirectory}/${stem}.split.bam \
-  -rf ReassignMappingQuality \
-  -DMQ 60 \
+  -rf ReassignOneMappingQuality \
+  -RMQF 255 -RMQT 60 \
   -U ALLOW_N_CIGAR_READS \
   -L chr1 -L chr2 -L chr3 -L chr4 -L chr5 -L chr6 -L chr7 -L chr8 -L chr9 -L chr10 -L chr11 -L chr12 \
   -L chr13 -L chr14 -L chr15 -L chr16 -L chr17 -L chr18 -L chr19 -L chr20 -L chr21 -L chr22 -L chrX -L chrY \"
