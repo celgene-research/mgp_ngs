5ec926a6bf83fd1daaa328da5fd87e80e425b51a
kmavrommatis <k_mavromatis@yahoo.gr>
2016-06-12 10:38:35 -0700

    Index a sorted bam file

---
+++ scripts_for_LSF/bin/995.bsub.buildBAMindex.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+scriptDir=$( dirname $0 ); source $scriptDir/../lib/shared.sh
+input=$1
+
+checkfile $input
+
+analysistask=52
+step="BuildBamIndex"
+
+
+NGS_CORE_FACTOR=2
+cores=1
+#necessary adjustment due to limitations in storage space on AWS instances
+
+memory=10000
+stem=$(fileStem $input)
+initiateJob $stem $step $1
+mkdir -p $NGS_LOG_DIR
+
+header=$(bsubHeader $stem $step $memory $cores)
+echo \
+"$header
+
+#$Date: 2015-08-14 13:02:55 -0700 (Fri, 14 Aug 2015) $ $Revision: 1624 $
+source $scriptDir/../lib/shared.sh
+
+initiateJob $stem $step $1
+set -e
+
+input=\$( stage.pl --operation out --type file  $input )
+if [ \$input == "FAILED"  ] ; then
+	echo \"Could not transfer \$input\"
+	exit 1
+fi
+
+
+outputDirectory=\$( setOutput \$input "bamfiles" )
+
+
+celgeneExec.pl --analysistask ${analysistask} \"\
+java -Xmx${memory}m -jar ${PICARDBASE}/picard.jar BuildBamIndex VERBOSITY=WARNING INPUT=\$input \
+  TMP_DIR=\${NGS_TMP_DIR}  VALIDATION_STRINGENCY=SILENT \"
+ if [ \$? != 0 ] ; then
+	echo "Failed to run command"
+	exit 1
+fi 
+
+ingestDirectory \$outputDirectory
+if [ \$? != 0 ] ; then
+	echo "Failed to ingest data"
+	exit 1
+fi 
+closeJob
+
+" > ${stem}.${step}.bsub
+
+bsub < ${stem}.${step}.bsub
+#rm $$.tmp
+
