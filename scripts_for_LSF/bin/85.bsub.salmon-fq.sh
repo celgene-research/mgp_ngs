57a72caaecb1bb878a01fa5128a1701075d97609
kmavrommatis <k_mavromatis@yahoo.gr>
2016-07-08 11:05:22 -0700

    Accept core dumps at the end

--- scripts_for_LSF/bin/85.bsub.salmon-fq.sh
+++ scripts_for_LSF/bin/85.bsub.salmon-fq.sh
@@ -35,7 +35,7 @@ stranded=$(ngs-sampleInfo.pl $input1 stranded)
 #R = read 1 (or single-end read) comes from the reverse strand
 
 
-transcriptsIndex=${ humansalmonidx }
+transcriptsIndex=${humansalmonidx}
 
 # end humansalmonidxof command arguments
 ##########################
@@ -46,24 +46,20 @@ header=$(bsubHeader $stem $step $memory $cores)
 echo \
 "$header
 
-#BSUB -E \"$scriptDir/../lib/stageReference.sh $step\"
-#$Date: 2015-08-14 13:02:55 -0700 (Fri, 14 Aug 2015) $ $Revision: 1624 $
-
-
-source $scriptDir/../lib/shared.sh 
-initiateJob $stem $step $1
+set +e # specifically used for salmon which dumps a core after it finishes
+ulimit -c 0
 
 input1=\$( stage.pl --operation out --type file  $input1 )
-input1unz=\$( echo \$input1 | sed 's/.gz//' )
+#input1unz=\$( echo \$input1 | sed 's/.gz//' )
 library=\"\"
 if [ \"$paired_end\" == \"1\" ]; then
 	library=\"I\"
 	input2=\$( stage.pl --operation out --type file  $input2 )
-	input2unz=\$( echo \$input2 | sed 's/.gz//' )
-	unzipCmd=\"$pigzbin -d \$input1 ; $pigzbin -d \$input2 \"
-	readCmd=\"-1 <(gunzip -c\$input1) -2 <(gunzip -c \$input2)\"
+	#input2unz=\$( echo \$input2 | sed 's/.gz//' )
+	#unzipCmd=\"$pigzbin -d \$input1 ; $pigzbin -d \$input2 \"
+	readCmd=\"-1 <(gunzip -c \$input1) -2 <(gunzip -c \$input2)\"
 else
-	library="\I\"
+	library=\"I\"
 	unzipCmd=\"$pigzbin -d \$input1 \"
 	readCmd=\"-r <(gunzip -c \$input1) \"
 fi
@@ -71,16 +67,18 @@ if [ \"$stranded\" == \"REVERSE\" ]; then
 	library=\$library\"SR\"
 fi
 if [ \"$stranded\" == \"NONE\" ]; then
-	library=\$library"\U\"
+	library=\$library\"U\"
 fi
 if [ \"$stranded\" == \"FORWARD\" ]; then
-	library=\$library"\SF\"
+	library=\$library\"SF\"
 fi
 
 outputDirectory=\$( setOutput \$input1 ${step}-transcriptCountsFastq )
 
 # no_bias_correct is used to avoid core dumps that happen frequently
 #\$unzipCmd ; \
+
+
 celgeneExec.pl --analysistask ${analysistask} \"\
 $salmonbin quant -i ${transcriptsIndex} \
   --libType '\$library' \
