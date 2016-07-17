4a023aefb0dc27531f38c1694b1499fb2ef91e33
kmavrommatis <k_mavromatis@yahoo.gr>
2016-07-17 00:16:16 -0700

    Fixed typo
    Removed second 'fi' from file

--- scripts_for_LSF/bin/210.bsub.Mutect2.sh
+++ scripts_for_LSF/bin/210.bsub.Mutect2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 scriptDir=$( dirname $0 ); source $scriptDir/../lib/shared.sh
+
 inputNormalBAM=$1
 inputTumorBAM=$2
 stem=$(fileStem $inputTumorBAM )
@@ -10,6 +11,52 @@ mkdir -p $NGS_LOG_DIR
 initiateJob $stem $step $1
 genomeDatabase=${humanGenomeDir}/genome.fa
 
+exomeSet=$(ngs-sampleInfo.pl $input bait_set)
+case  "${exomeSet}" in
+"Nextera_Rapid_Capture_v1.2_Illumina" )
+		baitsfile=${humanGenomeDir}/ExonCapture/nexterarapidcapture_exome_targetedregions_v1.2.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v1_38Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S0274956_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v2_44Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S0293689_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v3_50_Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S02972011_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v4_51Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S03723314_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v4+UTRs_71Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S03723424_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v5+UTRs_75Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S04380219_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v5_50Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S04380110_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v6+COSMIC_64Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S07604715_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v6_58Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S07604514_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v6+UTRs_58Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S07604624_Covered.intervals.bed
+	;;
+"Agilent_50_Mb_V5_with_extra_content" )
+	baitsfile=${humanGenomeDir}/ExonCapture/morgan.exomeplus.v5.padded.intervals.bed
+	;;
+"Agilent_50_Mb_v3_with_extra_content" )
+	baitsfile=${humanGenomeDir}/ExonCapture/morgan.exomeplus.v3.padded.intervals.bed
+	;;
+* )
+	echo "Cannot recognize exome capture kit"
+	;;
+esac
+captureKit=${exomeSet}
 
 memory=28000
 experimentType=$(ngs-sampleInfo.pl $inputNormalBAM experiment_type);
@@ -33,9 +80,12 @@ outputDirectory=\$( setOutput \$inputTumorBAM ${step} )
 
 
 
-
+# first create the intervals
+# second run ContEst
+# finally run Mutect2
 	
 celgeneExec.pl --analysistask $analysistask \"\
+
 java -Xmx${memory}m -jar ${gatkbin} \
 -T MuTect2 \
 -R ${genomeDatabase} \
@@ -45,13 +95,13 @@ java -Xmx${memory}m -jar ${gatkbin} \
 --cosmic ${cosmiccoding} \
 --dontUseSoftClippedBases \
 --output_mode EMIT_VARIANTS_ONLY \
+-L $captureKit  \
 -o \${outputDirectory}/${stem}.vcf \
 -nct $cores \"
 if [ \$? != 0 ] ; then
 	echo \"Failed to run command\"
 	exit 1
 fi 
-fi
 
 
 ingestDirectory \$outputDirectory
