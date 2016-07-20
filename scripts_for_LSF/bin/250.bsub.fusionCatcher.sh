9647bd8bcba415c8c89dccc38b1bd95a426c937b
kmavrommatis <k_mavromatis@yahoo.gr>
2016-07-20 16:49:08 -0700

    Fixed order of samples and the name of output

--- scripts_for_LSF/bin/250.bsub.fusionCatcher.sh
+++ scripts_for_LSF/bin/250.bsub.fusionCatcher.sh
@@ -3,25 +3,28 @@ scriptDir=$( dirname $0 ); source $scriptDir/../lib/shared.sh
 step="FusionCatcher"
 if [ -n "$1" ]
 then
-	input1=$1
+	inputControl1=$1
 else
 	echo "20.bsub.fusionCatcher.sh"
 	echo "script to distribute fusioncatcher jobs to the cluster"
 	echo "run using the following command format:"
-	echo "20.bsub.fusionCatcher.sh <INPUT_FASTQ (for a single sample or the tumor sample> <NORMAL FASTQ>"
+	echo "20.bsub.fusionCatcher.sh <INPUT_FASTQ (for a single sample or the control sample> <TUMOR FASTQ>"
 	echo "   if a pair of files is used for input (e.g. XXX_R1.fastq.gz and XXX_R2.fastq.gz)"
 	echo "   INPUT_FASTQ will be the first only e.g."
 	echo "   20.bsub.fusionCatcher.sh XXX_R1.fastq.gz"
 	exit;
 fi
 
-checkfile $input1
-stem=$(fileStem $input1)
-input2=$( getSecondReadFile $input1 )
+checkfile $inputControl1
+stem=$(fileStem $inputControl1)
+inputControl2=$( getSecondReadFile $inputControl1 )
+
 if [ -n "$2" ]
 then
-	inputControl1=$2
-	inputControl2=$( getSecondReadFile $inputControl1 )
+	inputTumor1=$2
+	inputTumor2=$( getSecondReadFile $inputTumor1 )
+	stemB=$(fileStem $inputTumor1)
+	stem=${stemB}-${stem}
 fi
 analysistask=63
 
@@ -39,24 +42,25 @@ echo \
 initiateJob $stem $step $1
 
 
-input1=\$( stage.pl --operation out --type file  $input1 )
-input2=\$( stage.pl --operation out --type file  $input2 )
-if [ -n "$inputControl1" ] ; then
-	inputControl1=\$( stage.pl --operation out --type file  $inputControl1 )
-	inputControl2=\$( stage.pl --operation out --type file  $inputControl2 )
-	cmdAdd=\" -I \$inputControl1,\$inputControl2 \"
+inputControl1=\$( stage.pl --operation out --type file  $inputControl1 )
+inputControl2=\$( stage.pl --operation out --type file  $inputControl2 )
+cmdAdd=\" -i \${inputControl1},\${inputControl2} \"
+if [ -n "$inputTumor1" ] ; then
+	inputTumor1=\$( stage.pl --operation out --type file  $inputTumor1 )
+	inputTumor2=\$( stage.pl --operation out --type file  $inputTumor2 )
+	cmdAdd=\" -i \$inputTumor1,\$inputTumor2 -I \$inputControl1,\$inputControl2 \"
 fi
 
-if [ \"\$input1\" == "FAILED" -o \"\$input2\" == "FAILED" -o  \"\$inputControl1\" == "FAILED" -o \"\$inputControl2\" == "FAILED"] ; then
+if [ \"\$inputControl1\" == "FAILED" -o \"\$inputControl2\" == "FAILED" -o  \"\$inputTumor1\" == "FAILED" -o \"\$inputTumor2\" == "FAILED"] ; then
 	echo "Could not transfer  one of the fastq files"
 	exit 1
 fi
 
-outputDirectory=\$( setOutput \$input1 ${step} )
+outputDirectory=\$( setOutput \$inputControl1 ${step} )
 
 celgeneExec.pl --analysistask ${analysistask} \"$fusioncatcherbin \
 -d \$fusioncatcheridx \
--i \${input1},\${input2} \$cmdAdd\
+ \$cmdAdd \
 -o \${outputDirectory}/$stem.strvar \
 -p ${cores} \
 -V  \"
