e3ba624e8b636ef5c0ae93308ffdfef1ae09095b
kmavrommatis <k_mavromatis@yahoo.gr>
2016-07-20 11:15:06 -0700

    Added the option for multiple threads in the list of arguments

--- scripts_for_LSF/bin/250.bsub.fusionCatcher.sh
+++ scripts_for_LSF/bin/250.bsub.fusionCatcher.sh
@@ -8,21 +8,25 @@ else
 	echo "20.bsub.fusionCatcher.sh"
 	echo "script to distribute fusioncatcher jobs to the cluster"
 	echo "run using the following command format:"
-	echo "20.bsub.fusionCatcher.sh <INPUT_FASTQ>"
+	echo "20.bsub.fusionCatcher.sh <INPUT_FASTQ (for a single sample or the tumor sample> <NORMAL FASTQ>"
 	echo "   if a pair of files is used for input (e.g. XXX_R1.fastq.gz and XXX_R2.fastq.gz)"
 	echo "   INPUT_FASTQ will be the first only e.g."
 	echo "   20.bsub.fusionCatcher.sh XXX_R1.fastq.gz"
 	exit;
 fi
+
 checkfile $input1
 stem=$(fileStem $input1)
 input2=$( getSecondReadFile $input1 )
-
+if [ -n "$2" ]
+then
+	inputControl1=$2
+	inputControl2=$( getSecondReadFile $inputControl1 )
+fi
 analysistask=63
 
 initiateJob $stem $step $1
 
-database=/celgene/software/fusionCatcher/data/current
 cores=$(fullcores)
 
 
@@ -37,10 +41,14 @@ initiateJob $stem $step $1
 
 input1=\$( stage.pl --operation out --type file  $input1 )
 input2=\$( stage.pl --operation out --type file  $input2 )
+if [ -n "$inputControl1" ] ; then
+	inputControl1=\$( stage.pl --operation out --type file  $inputControl1 )
+	inputControl2=\$( stage.pl --operation out --type file  $inputControl2 )
+	cmdAdd=\" -I \$inputControl1,\$inputControl2 \"
+fi
 
-
-if [ \$database == "FAILED" -o \$input1 == "FAILED" -o \$input2 == "FAILED" ] ; then
-	echo "Could not transfer either \$database or \$input1 or \$input2"
+if [ \"\$input1\" == "FAILED" -o \"\$input2\" == "FAILED" -o  \"\$inputControl1\" == "FAILED" -o \"\$inputControl2\" == "FAILED"] ; then
+	echo "Could not transfer  one of the fastq files"
 	exit 1
 fi
 
@@ -48,14 +56,16 @@ outputDirectory=\$( setOutput \$input1 ${step} )
 
 celgeneExec.pl --analysistask ${analysistask} \"$fusioncatcherbin \
 -d \$fusioncatcheridx \
--i \${input1},\${input2} \
--o \${outputDirectory}\"
+-i \${input1},\${input2} \$cmdAdd\
+-o \${outputDirectory}/$stem.strvar \
+-p ${cores} \
+-V  \"
 
 if [ \$? != 0 ] ; then
 	echo "Failed to run command"
 	exit 1
 fi 
-ingestDirectory \$outputDirectory
+ingestDirectory \$outputDirectory yes
 if [ \$? != 0 ] ; then
 	echo "Failed to ingest data"
 	exit 1
