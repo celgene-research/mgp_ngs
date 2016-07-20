2b6ce538db8fe2f7d4c652b95e58cecd373585a1
kmavrommatis <k_mavromatis@yahoo.gr>
2016-07-19 17:32:38 -0700

    Initial import script for fusioncatcher

---
+++ scripts_for_LSF/bin/250.bsub.fusionCatcher.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+scriptDir=$( dirname $0 ); source $scriptDir/../lib/shared.sh
+step="FusionCatcher"
+if [ -n "$1" ]
+then
+	input1=$1
+else
+	echo "20.bsub.fusionCatcher.sh"
+	echo "script to distribute fusioncatcher jobs to the cluster"
+	echo "run using the following command format:"
+	echo "20.bsub.fusionCatcher.sh <INPUT_FASTQ>"
+	echo "   if a pair of files is used for input (e.g. XXX_R1.fastq.gz and XXX_R2.fastq.gz)"
+	echo "   INPUT_FASTQ will be the first only e.g."
+	echo "   20.bsub.fusionCatcher.sh XXX_R1.fastq.gz"
+	exit;
+fi
+checkfile $input1
+stem=$(fileStem $input1)
+input2=$( getSecondReadFile $input1 )
+
+analysistask=63
+
+initiateJob $stem $step $1
+
+database=/celgene/software/fusionCatcher/data/current
+cores=$(fullcores)
+
+
+memory=20000
+header=$(bsubHeader $stem $step $memory $cores)
+echo \
+"$header
+
+
+initiateJob $stem $step $1
+
+
+input1=\$( stage.pl --operation out --type file  $input1 )
+input2=\$( stage.pl --operation out --type file  $input2 )
+
+
+if [ \$database == "FAILED" -o \$input1 == "FAILED" -o \$input2 == "FAILED" ] ; then
+	echo "Could not transfer either \$database or \$input1 or \$input2"
+	exit 1
+fi
+
+outputDirectory=\$( setOutput \$input1 ${step} )
+
+celgeneExec.pl --analysistask ${analysistask} \"$fusioncatcherbin \
+-d \$fusioncatcheridx \
+-i \${input1},\${input2} \
+-o \${outputDirectory}\"
+
+if [ \$? != 0 ] ; then
+	echo "Failed to run command"
+	exit 1
+fi 
+ingestDirectory \$outputDirectory
+if [ \$? != 0 ] ; then
+	echo "Failed to ingest data"
+	exit 1
+fi 
+
+rm -rf \$outputDirectory
+
+closeJob
+
+"\
+>${stem}.${step}.$( getStdSuffix ).bsub
+
+
+bsub < ${stem}.${step}.$( getStdSuffix ).bsub
+#rm $jobDesc.bsub
+
