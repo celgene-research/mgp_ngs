52bd7aa46db00f5e44cb64205a7b0ce8d0145671
kmavrommatis <k_mavromatis@yahoo.gr>
2016-07-23 10:24:57 -0700

    Script handles WES data

--- scripts_for_LSF/bin/680.bsub.controlFreec.sh
+++ scripts_for_LSF/bin/680.bsub.controlFreec.sh
@@ -41,7 +41,54 @@ else
 	mateorientation="0"
 fi
 
-configTemplate="680.template.controlFreec.WGS-NT.txt"
+exomeSet=$(ngs-sampleInfo.pl $input bait_set)
+case  "${exomeSet}" in
+"Nextera_Rapid_Capture_v1.2_Illumina" )
+		baitsfile=${humanGenomeDir}/ExonCapture/nexterarapidcapture_exome_targetedregions_v1.2.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v1_38Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S0274956_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v2_44Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S0293689_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v3_50_Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S02972011_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v4_51Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S03723314_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v4+UTRs_71Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S03723424_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v5+UTRs_75Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S04380219_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v5_50Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S04380110_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v6+COSMIC_64Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S07604715_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v6_58Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S07604514_Covered.intervals.bed
+	;;
+"SureSelect_Human_All_exon_v6+UTRs_58Mb_Agilent" )
+	baitsfile=${humanGenomeDir}/ExonCapture/S07604624_Covered.intervals.bed
+	;;
+"Agilent_50_Mb_V5_with_extra_content" )
+	baitsfile=${humanGenomeDir}/ExonCapture/morgan.exomeplus.v5.padded.intervals.bed
+	;;
+"Agilent_50_Mb_v3_with_extra_content" )
+	baitsfile=${humanGenomeDir}/ExonCapture/morgan.exomeplus.v3.padded.intervals.bed
+	;;
+* )
+	echo "Cannot recognize exome capture kit"
+	;;
+esac
+
+
+configTemplate="680.template.controlFreec.WES-NT.txt"
 
 
 mkdir -p $NGS_LOG_DIR
@@ -64,8 +111,8 @@ if [ \$inputsample == \"FAILED\" ]; then
 fi
 outputDirectory=\$( setOutput \$inputsample ${step} )
 
-
-
+bedfile=\$outputDirectory/\$(basename $baitsfile)
+grep -f '^@' $baitsfile > \$bedfile
 
 source ${scriptDir}/${configTemplate} > \$outputDirectory/${stem}-${step}.config
 
