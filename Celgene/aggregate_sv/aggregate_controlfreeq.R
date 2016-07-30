33f665ed39d5ab5e8abfa57153de62326a37167f
Fadi Towfic <ftowfic@ip-192-168-10-4.us-west-1.compute.internal>
2016-07-30 06:09:44 -0700

    updating the aggregation code for controlfreeq to make it more memory efficient

--- Celgene/aggregate_sv/aggregate_controlfreeq.R
+++ Celgene/aggregate_sv/aggregate_controlfreeq.R
@@ -1,20 +1,29 @@
 library(utils)
 
-read.dir <- function(dirpath,label) {
-  read.ratio <- function(X) {
+read.dir <- function(dirpath,label,output) {
+  read.ratio <- function(X,output) {
     tab <- read.delim(file=X,header=T,as.is=T,check.names=F,colClasses=c('character'))
     tab[['DATASET']] <- rep(label,nrow(tab));
     tab[['SAMPLE']] <- rep(basename(X),nrow(tab))
-    tab
+    write.table(tab,row.names=F,append = T,col.names=F,sep=',')
   }
-  directories <- dir(path = dirpath,pattern='*.strvar',full.names = T)
+  directories <- dir(path = dirpath,pattern='*.strvar',full.names = T)[1]
   files <- dir(path = directories,pattern='*.gz_ratio.txt',full.names=T)
-  lapply(files,read.ratio)
+  lapply(files,read.ratio,output=output)
 }
 
-dfci <- read.dir(dirpath = '/mnt/celgene.rnd.combio.mmgp.external/SeqData/WES/ProcessedData/DFCI/controlFreec.human_1469503404',label = 'DFCI')
-mmrf <- read.dir(dirpath = '/mnt/celgene.rnd.combio.mmgp.external/SeqData/WES/ProcessedData/MMRF/controlFreec.human_1469724160',label = 'MMRF')
-uams <- read.dir(dirpath = '/mnt/celgene.rnd.combio.mmgp.external/SeqData/WES/ProcessedData/UAMS/controlFreec.human_1469503981',label = 'UAMS')
+writeHeader <- function(dir,output) {
+  directories <- dir(path = dirpath,pattern='*.strvar',full.names = T)
+  files <- dir(path = directories,pattern='*.gz_ratio.txt',full.names=T)
+  template <- files[1];
+  tab <- read.delim(file=template,header=T,as.is=T,check.names=F,colClasses=c('character'))
+  tab[['DATASET']] <- rep('DATASET',nrow(tab));
+  tab[['SAMPLE']] <- rep('SAMPLE',nrow(tab))
+  write.table(t(colnames(tab)),file='/home/ftowfic/test.csv',row.names=F,col.names =F,sep=',')
+}
 
-out <- do.call(rbind,list(dfci,mmrf,uams))
-write.csv(out,file='/mnt/celgene.rnd.combio.mmgp.external/SeqData/WES/ProcessedData/controlFreeq_combined_dfci_mmrf_uams.csv',row.names=F)
+output <- '/mnt/celgene.rnd.combio.mmgp.external/SeqData/WES/ProcessedData/controlFreeq_combined_dfci_mmrf_uams.csv';
+writeHeader(dirpath = '/mnt/celgene.rnd.combio.mmgp.external/SeqData/WES/ProcessedData/DFCI/controlFreec.human_1469503404',output)
+dfci <- read.dir(dirpath = '/mnt/celgene.rnd.combio.mmgp.external/SeqData/WES/ProcessedData/DFCI/controlFreec.human_1469503404',label = 'DFCI',output = output)
+mmrf <- read.dir(dirpath = '/mnt/celgene.rnd.combio.mmgp.external/SeqData/WES/ProcessedData/MMRF/controlFreec.human_1469724160',label = 'MMRF',output=output)
+uams <- read.dir(dirpath = '/mnt/celgene.rnd.combio.mmgp.external/SeqData/WES/ProcessedData/UAMS/controlFreec.human_1469503981',label = 'UAMS',output=output)
