3787fd0231c6dd3ef76564f028fed342ee16395a
Fadi Towfic <ftowfic@ip-192-168-10-4.us-west-1.compute.internal>
2016-07-30 06:17:16 -0700

    fixing some bugs in the aggregate_controlfreeq.R code

--- Celgene/aggregate_sv/aggregate_controlfreeq.R
+++ Celgene/aggregate_sv/aggregate_controlfreeq.R
@@ -5,14 +5,14 @@ read.dir <- function(dirpath,label,output) {
     tab <- read.delim(file=X,header=T,as.is=T,check.names=F,colClasses=c('character'))
     tab[['DATASET']] <- rep(label,nrow(tab));
     tab[['SAMPLE']] <- rep(basename(X),nrow(tab))
-    write.table(tab,row.names=F,append = T,col.names=F,sep=',')
+    write.table(tab,file=output,row.names=F,append = T,col.names=F,sep=',')
   }
-  directories <- dir(path = dirpath,pattern='*.strvar',full.names = T)[1]
+  directories <- dir(path = dirpath,pattern='*.strvar',full.names = T)
   files <- dir(path = directories,pattern='*.gz_ratio.txt',full.names=T)
   lapply(files,read.ratio,output=output)
 }
 
-writeHeader <- function(dir,output) {
+writeHeader <- function(dirpath,output) {
   directories <- dir(path = dirpath,pattern='*.strvar',full.names = T)
   files <- dir(path = directories,pattern='*.gz_ratio.txt',full.names=T)
   template <- files[1];
