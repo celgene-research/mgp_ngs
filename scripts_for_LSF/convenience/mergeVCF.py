cbc5d8ebc9e3fe628552f9899c0e5f4a9324c86e
kmavrommatis <k_mavromatis@yahoo.gr>
2016-08-16 15:23:17 -0700

    test

--- scripts_for_LSF/convenience/mergeVCF.py
+++ scripts_for_LSF/convenience/mergeVCF.py
@@ -28,7 +28,7 @@ try: args.directory
 except NameError: ars.directory=None
 else:
     vcfDirectory=args.directory
-    logging.info("BAM directory set to " + vcfDirectory)
+    logging.info("VCF directory set to " + vcfDirectory)
 
 try: args.temp
 except NameError: args.temp=None
@@ -72,27 +72,27 @@ if outputFname is None:
 
 #bamDirectory="/mnt/celgene-src-bucket/DA0000096/ChIP-Seq-2/Processed/Bowtie2.human-bamfile-Mdup_1447913850_1456862015"
 
-files=os.listdir(vcfDirectory , )
 regex=re.compile( mask+'$')
 regex2=re.compile(".gz$")
-for f in files:
-    full_path=os.path.join( vcfDirectory, f)
-    full_path=full_path.replace("/mnt/","s3://")
-   
-    if re.search(regex, full_path ):
-        # we copy the file to the temporary directory and gbzip/tabix if needed
-        logging.info("Copying file "+f+" to the temporary directory")
-        #copyProcess=subprocess.check_output(["aws s3 cp", full_path, "  ", tempDirectory+"/"+f] )
-        if(not re.search(regex2, f)):
-            logging.info("This file will be compressed.")
-            #compressProcess=subprocess.check_output(["bgzip "+f] )
-        logging.info("This file seems to be already compressed. Will create the tabix index")
-        #indexProcess=subprocess.check_output(["tabix -p vcf "+f+".gz"] )
-        
-        
-        
-        #print full_path  +  " " + target + " " + cell_line
-  
+for root,dirs,files in os.walk(vcfDirectory , ):
+    for f in files :
+        full_path=os.path.join( root, dirs, f)
+        full_path=full_path.replace("/mnt/","s3://")
+       
+        if re.search(regex, f):
+            # we copy the file to the temporary directory and gbzip/tabix if needed
+            logging.info("Copying file "+f+" to the temporary directory")
+            #copyProcess=subprocess.check_output(["aws s3 cp", full_path, "  ", tempDirectory+"/"+f] )
+            if(not re.search(regex2, f)):
+                logging.info("This file will be compressed.")
+                #compressProcess=subprocess.check_output(["bgzip "+f] )
+            logging.info("This file seems to be already compressed. Will create the tabix index")
+            #indexProcess=subprocess.check_output(["tabix -p vcf "+f+".gz"] )
+            
+            
+            
+            #print full_path  +  " " + target + " " + cell_line
+      
 exit(0)
 
 
