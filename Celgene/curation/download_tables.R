d90d6d2b2fb2f596801404e40e889bff40b0f2eb
dkrozelle <dan.rozelle@gmail.com>
2016-11-09 08:09:13 -0800

    revise to limit unnecessary file transfer

--- Celgene/curation/download_tables.R
+++ Celgene/curation/download_tables.R
@@ -1,20 +1,28 @@
-s3clinical      <- "s3://celgene.rnd.combio.mmgp.external/ClinicalData"
-local_path      <- "~/thindrives/mmgp/data/curated"
 
+# simplified scripts to save s3 files to locally mounted storage
+s3clinical      <- "s3://celgene.rnd.combio.mmgp.external/ClinicalData"
+local_path      <- "~/thindrives/mmgp/data"
 if(!dir.exists(local_path)){warning("local drive not mounted")}
 
+# We are editing the dictionary spreadsheet on EC2, so push latest to s3 before download
+system(  paste('aws s3 cp',"mgp_dictionary.xlsx" , file.path(s3clinical, "ProcessedData", "Integrated", "mgp_dictionary.xlsx"), "--sse ", sep = " "))
 
-# We are editing the dictionary spreadsheet locally, so push latest to s3
-files <- system(  paste('aws s3 ls',file.path(s3clinical,"ProcessedData","Integrated/"), sep = " "), intern = T)
+###
+# this script is meant to be run line-by-line as needed after uncommenting to prevent bulk transfer
 
-#trim to filename, this assumes no spaces in filename
-files <- gsub("^.* (.*)","\\1",files)
+# all ORIGINAL data
+# system(  paste('aws s3 cp', file.path(s3clinical, "OriginalData"), 
+#                             file.path(local_path, "OriginalData"), '--recursive', sep = " "))
 
-file_includes <- '--exclude "*"'
-file_includes <- paste(file_includes, "--include", tail(grep("PER-FILE", files, value = T), n=1), sep = " ")
-file_includes <- paste(file_includes, "--include", tail(grep("PER-PATIENT", files, value = T), n=1), sep = " ")
-file_includes <- paste(file_includes, "--include", tail(grep("dictionary", files, value = T), n=1), sep = " ")
-file_includes
+# all individual PROCESSED data tables
+# system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData"),
+#                             file.path(local_path, "ProcessedData"),
+#                '--recursive --exclude "*"',
+#                '--include "DFCI*" --include "MMRF_IA9*" --include "UAMS*"',
+#                '--include "JointData*"', sep = " "))
 
-# get the most recent integrated tables
-system(  paste('aws s3 cp',file.path(s3clinical,'ProcessedData', "Integrated"), file.path(local_path, "Integrated"), '--recursive', file_includes, sep = " "))
+#  INTEGRATED tables
+# system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated"),
+#                file.path(local_path),
+#                '--recursive --exclude "Archive*" --exclude "mgp-shiny*" --exclude "sas*"',
+#                sep = " "))
