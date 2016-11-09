b15d25f03c231b9f8579ebeba0b817e3020244a3
dkrozelle <dan.rozelle@gmail.com>
2016-11-09 07:13:46 -0800

    revise s3 get lines to be more specific; remove table merge function calls

--- Celgene/curation/mgp_clinical_aggregation.R
+++ Celgene/curation/mgp_clinical_aggregation.R
@@ -22,27 +22,29 @@ source("qc_and_summary.R")
 
 # locations
 s3clinical      <- "s3://celgene.rnd.combio.mmgp.external/ClinicalData"
-integrated_path <- file.path(s3clinical,"ProcessedData", "Integrated")
-processed_path  <- file.path(s3clinical,"ProcessedData")
 local_path      <- "/tmp/curation"
 if(!dir.exists(local_path)){dir.create(local_path)}
 
 # We are editing the dictionary spreadsheet locally, so push latest to s3
-system(  paste('aws s3 cp',"mgp_dictionary.xlsx" , file.path(integrated_path, "mgp_dictionary.xlsx"), "--sse ", sep = " "))
+system(  paste('aws s3 cp',"mgp_dictionary.xlsx" , file.path(s3clinical, "ProcessedData", "Integrated", "mgp_dictionary.xlsx"), "--sse ", sep = " "))
+
 
 ### if appropriate, run curation scripts
-source("curate_DFCI.R")
-# source("curate_MMRF.R")
+# source("curate_DFCI.R")
+# source("curate_MMRF_IA9.R")
 # source("curate_UAMS.R")
-# source("../aggregate_stats/uams_calls_to_binary_table.R")
+# source("curate_joint_datasets.R")
 
-# copy files locally
-# dictionary, curated files
-system(  paste('aws s3 cp', processed_path, local_path, '--recursive --exclude "*" --include "*dictionary*" --include "*curated*"', sep = " "))
+# copy curated files locally
+system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated", "mgp_dictionary.xlsx"), local_path, sep = " "))
+system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "DFCI"), local_path, '--recursive --exclude "*" --include "curated*"', sep = " "))
+system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "MMRF_IA9"), local_path, '--recursive --exclude "*" --include "curated*"', sep = " "))
+system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "UAMS"), local_path, '--recursive --exclude "*" --include "curated*"', sep = " "))
+system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Joint_Datasets/curated_All_translocation_Summaries_from_BWalker_2016-10-04_zeroed_dkr.txt"), local_path, sep = " "))
 
 ##############
 # The dictionary is used as a starting framework for each level table
-dict <- as.data.frame(readxl::read_excel(file.path(local_path, "Integrated", "mgp_dictionary.xlsx")))
+dict <- as.data.frame(readxl::read_excel(file.path(local_path, "mgp_dictionary.xlsx")))
 dict <- dict[dict$active == 1,]
 
 files <- list.files(local_path, pattern = "curated*", full.names = T, recursive = T)
@@ -62,7 +64,7 @@ for(f in files){
 
   # qc and summary    
   per.file <- remove_invalid_samples(per.file)
-  per.file <- translocation_consensus_building(tmp)
+  per.file <- translocation_consensus_building(per.file)
 
   # write the aggregated table to local
   name <- paste("PER-FILE_clinical_cyto", ".txt", sep = "")
@@ -90,27 +92,13 @@ names(per.patient) <- patient_level_columns
   path <- file.path(local,name)
   write.table(per.patient, path, row.names = F, col.names = T, sep = "\t", quote = F)
 
-  
-#######################
-# merge final clinical tables with desired summary analysis tables
-# CNV
-  
- tmp <-  merge_table_files(file.path(local,"PER-FILE_clinical_cyto.txt"),
-                    file.path(local,"Integrated","curated_CNV_ControlFreec.txt")
-                    )
-  path <- file.path(local,"PER-FILE_clinical_cyto_cnv.txt")
-  write.table(tmp, path, row.names = F, col.names = T, sep = "\t", quote = F)
-  
+
   
 #######################
 # put final integrated files back as ProcessedData/Integrated on S3
-processed <- file.path(s3clinical,"ProcessedData","Integrated")
-system(  paste('aws s3 cp', local, processed, '--recursive --exclude "*" --include "PER*" --sse', sep = " "))
+system(  paste('aws s3 cp', local, file.path(s3clinical, "ProcessedData", "Integrated"), '--recursive --exclude "*" --include "PER*" --sse', sep = " "))
 return_code <- system('echo $?', intern = T)
   
 # clean up source files
-rm(per.file, per.patient, tmp)
+rm(per.file, per.patient)
 if(return_code == "0") system(paste0("rm -r ", local))
-
-# save a copy to my local drive for inspection
-source("download_tables.R")
