0d7593424618ade03426ac0a27439231eb0f6603
dkrozelle <dan.rozelle@gmail.com>
2016-11-16 09:45:00 -0800

    add aggregation lines to convert per-file to per-patient data

--- Celgene/curation/table_merge.R
+++ Celgene/curation/table_merge.R
@@ -17,7 +17,7 @@ system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "JointData"),
                            file2 = file.path(local_path,"PER-PATIENT_clinical.txt"),
                            id = c("Patient", "Study"))
   write.table(df, file.path(local_path,"tmp.txt"), row.names = F, col.names = T, sep = "\t", quote = F)
-
+  minimal_col_names <- names(df)
 #######################
 # CNV
   df <-  merge_table_files(file1 = file.path(local_path,"tmp.txt"),
@@ -44,7 +44,42 @@ name <- "PER-FILE_ALL.txt"
 system(  paste('aws s3 cp', file.path(local_path,"tmp.txt"), file.path(s3clinical, "ProcessedData", "Integrated", name), '--sse', sep = " "))
 return_code <- system('echo $?', intern = T)
 
-# as a failsafe to prevent reading older versions of source files remove the
-#  cached version file if transfer was successful.
-if(return_code == "0") system(paste0("rm -r ", local))
 
+#######################
+# collapse_file_to_patient
+#  this needs to reduce the PER-FILE organized table such that it contains a single *ND Tumor* row for each patient
+
+# df <- read.delim(file.path(local_path,"tmp.txt"), stringsAsFactors = F, as.is = T, check.names = F)
+  df <- df[df$Sample_Type_Flag == 1 & df$Disease_Status == "ND",]
+  
+  df <- aggregate.data.frame(df, by = list(df$Patient), function(x){
+    x <- unique(x)
+    x <- x[!is.na(x)]
+    if(length(x) > 1){print(x)}
+    paste(x, collapse = "; ")
+    })
+  
+  df[,c("Group.1","File_Name" ,"File_Name_Actual","File_Path")] <- NULL
+  
+  
+  # write to local and S3
+  name <- "PER-PATIENT_nd_tumor_ALL.txt"
+  path <- file.path(local,name)
+  write.table(df, path, row.names = F, col.names = T, sep = "\t", quote = F)
+  system(  paste('aws s3 cp', path, file.path(s3clinical, "ProcessedData", "Integrated", name), '--sse', sep = " "))
+  
+  # subset to a minimal clinical data table
+  df <- df[, minimal_col_names[minimal_col_names %in% names(df)]]
+  name <- "PER-PATIENT_nd_tumor_clinical.txt"
+  path <- file.path(local,name)
+  write.table(df, path, row.names = F, col.names = T, sep = "\t", quote = F)
+  system(  paste('aws s3 cp', path, file.path(s3clinical, "ProcessedData", "Integrated", name), '--sse', sep = " "))
+  
+    
+    
+    
+  return_code <- system('echo $?', intern = T)
+  if(return_code == "0"){ 
+    system(paste0("rm -r ", local))
+    rm(df)
+    }
