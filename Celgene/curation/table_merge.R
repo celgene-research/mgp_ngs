71f3dae505b7c4226cb4604a0419614c2118a24e
dkrozelle <dan.rozelle@gmail.com>
2016-11-16 08:01:40 -0800

    add biallelic inactivation curation

--- Celgene/curation/table_merge.R
+++ Celgene/curation/table_merge.R
@@ -11,29 +11,33 @@ source("curation_scripts.R")
 system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated"), local_path, '--recursive --exclude "*" --include "PER*"', sep = " "))
 system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "JointData"), local_path, '--recursive --exclude "*" --include "curated*"', sep = " "))
 
+#######################
+# add patient level information, this will be redundant
+  df <-  merge_table_files(file1 = file.path(local_path,"PER-FILE_clinical_cyto.txt"),
+                           file2 = file.path(local_path,"PER-PATIENT_clinical.txt"),
+                           id = c("Patient", "Study"))
+  write.table(df, file.path(local_path,"tmp.txt"), row.names = F, col.names = T, sep = "\t", quote = F)
 
 #######################
 # CNV
-  df <-  merge_table_files(file1 = file.path(local_path,"PER-FILE_clinical_cyto.txt"),
+  df <-  merge_table_files(file1 = file.path(local_path,"tmp.txt"),
                          file2 = file.path(local_path,"curated_CNV_ControlFreec.txt"),
                                            id = "File_Name")
   write.table(df, file.path(local_path,"tmp.txt"), row.names = F, col.names = T, sep = "\t", quote = F)
 
+#######################
+# Biallelic Inactivation Flags
+  df <-  merge_table_files(file1 = file.path(local_path,"tmp.txt"),
+                           file2 = file.path(local_path,"curated_BiallelicInactivation_Flag.txt"),
+                           id = "File_Name")
+  write.table(df, file.path(local_path,"tmp.txt"), row.names = F, col.names = T, sep = "\t", quote = F)
+  
 #######################
 # SNV
 df <-  merge_table_files(file1 = file.path(local_path,"tmp.txt"),
                          file2 = file.path(local_path,"curated_SNV_mutect2.txt"),
                          id = "File_Name")
 write.table(df, file.path(local_path,"tmp.txt"), row.names = F, col.names = T, sep = "\t", quote = F)
-
-  
-#######################
-# add patient level information, this will be redundant
-df <-  merge_table_files(file1 = file.path(local_path,"tmp.txt"),
-                         file2 = file.path(local_path,"PER-PATIENT_clinical.txt"),
-                         id = c("Patient", "Study"))
-write.table(df, file.path(local_path,"tmp.txt"), row.names = F, col.names = T, sep = "\t", quote = F)
-
 
 # put back the all table
 name <- "PER-FILE_ALL.txt"
