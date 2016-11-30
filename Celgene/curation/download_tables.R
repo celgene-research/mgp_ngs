759a9ec8c7549745f183ad91f14a82c1cdf6e748
dkrozelle <dan.rozelle@gmail.com>
2016-11-30 12:04:03 -0800

    add

--- Celgene/curation/download_tables.R
+++ Celgene/curation/download_tables.R
@@ -4,9 +4,6 @@ s3clinical      <- "s3://celgene.rnd.combio.mmgp.external/ClinicalData"
 local_path      <- "~/thindrives/mmgp/data"
 if(!dir.exists(local_path)){warning("local drive not mounted")}
 
-# We are editing the dictionary spreadsheet on EC2, so push latest to s3 before download
-system(  paste('aws s3 cp',"mgp_dictionary.xlsx" , file.path(s3clinical, "ProcessedData", "Integrated", "mgp_dictionary.xlsx"), "--sse ", sep = " "))
-
 ###
 # this script is meant to be run line-by-line as needed after uncommenting to prevent bulk transfer
 
@@ -15,18 +12,19 @@ system(  paste('aws s3 cp',"mgp_dictionary.xlsx" , file.path(s3clinical, "Proces
 #                             file.path(local_path, "OriginalData"), '--recursive', sep = " "))
 
 # all individual PROCESSED data tables
-# system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData"),
-#                             file.path(local_path, "ProcessedData"),
-#                '--recursive --exclude "*"',
-#                '--include "DFCI*" --include "MMRF_IA9*" --include "UAMS*"',
-#                '--include "JointData*"', sep = " "))
+system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData"),
+                            file.path(local_path, "ProcessedData"),
+               '--recursive --exclude "*"',
+               # '--include "DFCI*" --include "MMRF_IA9*" --include "UAMS*"',
+               '--include "JointData*"', sep = " "))
 
-#  INTEGRATED tables
-system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated"),
-               file.path(local_path),
-               '--recursive --exclude "Archive*" --exclude "mgp-shiny*" --exclude "sas*"',
-               '--include "PER*"',
-               sep = " "))
+#  INTEGRATED tables, push dictionary before download
+# system(  paste('aws s3 cp',"mgp_dictionary.xlsx" , file.path(s3clinical, "ProcessedData", "Integrated", "mgp_dictionary.xlsx"), "--sse ", sep = " "))
+# system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated"),
+#                file.path(local_path),
+#                '--recursive --exclude "Archive*" --exclude "mgp-shiny*" --exclude "sas*"',
+#                '--include "PER*"',
+#                sep = " "))
 
 #  report and summary tables
 # system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated"),
@@ -36,8 +34,8 @@ system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated")
 #                sep = " "))
 
 #  sas tables
-system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated"),
-               file.path(local_path),
-               '--recursive --exclude "*"',
-               '--include "sas*"',
-               sep = " "))
+# system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated"),
+#                file.path(local_path),
+#                '--recursive --exclude "*"',
+#                '--include "sas*"',
+#                sep = " "))
