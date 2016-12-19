2424842056b60c7bf24d56ce889cd4b154cb7512
dkrozelle <dan.rozelle@gmail.com>
2016-12-19 09:41:09 -0800

    update fetch syntax

--- Celgene/curation/download_tables.R
+++ Celgene/curation/download_tables.R
@@ -12,11 +12,11 @@ if(!dir.exists(local_path)){warning("local drive not mounted")}
 #                             file.path(local_path, "OriginalData"), '--recursive', sep = " "))
 
 # all individual PROCESSED data tables
-system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData"),
-                            file.path(local_path, "ProcessedData"),
-               '--recursive --exclude "*"',
-               # '--include "DFCI*" --include "MMRF_IA9*" --include "UAMS*"',
-               '--include "JointData*"', sep = " "))
+# system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData"),
+#                             file.path(local_path, "ProcessedData"),
+#                '--recursive --exclude "*"',
+#                '--include "DFCI*" --include "MMRF_IA9*" --include "UAMS*"',
+#                '--include "JointData*"', sep = " "))
 
 #  INTEGRATED tables, push dictionary before download
 # system(  paste('aws s3 cp',"mgp_dictionary.xlsx" , file.path(s3clinical, "ProcessedData", "Integrated", "mgp_dictionary.xlsx"), "--sse ", sep = " "))
@@ -34,8 +34,8 @@ system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData"),
 #                sep = " "))
 
 #  sas tables
-# system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated"),
-#                file.path(local_path),
-#                '--recursive --exclude "*"',
-#                '--include "sas*"',
-#                sep = " "))
+system(  paste('aws s3 cp', file.path(s3clinical, "ProcessedData", "Integrated"),
+               file.path(local_path),
+               '--recursive --exclude "*"',
+               '--include "sas*"',
+               sep = " "))
