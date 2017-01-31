e3cc3f0668634ae2dcb5f17b4b6b9606a50c5ec1
dkrozelle <dan.rozelle@gmail.com>
2017-01-31 10:27:58 -0800

    remove local collapse df function

--- Celgene/curation/table_merge.R
+++ Celgene/curation/table_merge.R
@@ -22,34 +22,11 @@ table_merge <- function(per.file, per.patient){
   #######################
   # SNV
   # print("SNV Merge........................................", quote = F)
-  snv <- GetS3Table(file.path(s3joint,"curated_SNV_mutect2.txt"))
-  df <-  merge_table_files(df1 = df, df2 = snv, id = "File_Name")
+  # snv <- GetS3Table(file.path(s3joint,"curated_SNV_mutect2.txt"))
+  # df <-  merge_table_files(df1 = df, df2 = snv, id = "File_Name")
   
   return(df)
   }
-
-collapse_to_patient <- function(df){
-  #######################
-  # collapse_file_to_patient
-  #  this needs to reduce the PER-FILE organized table such that it contains a single *ND Tumor* row for each patient
-  
-  # df <- read.delim(file.path(local_path,"tmp.txt"), stringsAsFactors = F, as.is = T, check.names = F)
-  df <- df[df$Sample_Type_Flag == 1 & df$Disease_Status == "ND",]
-  
-  df <- aggregate.data.frame(df, by = list(df$Patient), function(x){
-    x <- unique(x)
-    x <- x[!is.na(x)]
-    # if(length(x) > 1){print(x)}
-    paste(x, collapse = "; ")
-  })
-  
-  df[,c("Group.1","File_Name" ,"File_Name_Actual","File_Path")] <- NULL
-  
-  # write to S3
-  write_to_s3integrated(df, name = "PER-PATIENT_nd_tumor_ALL.txt")
-
-  return(df)
-}
 
 subset_clinical_columns <- function(df){
   # remove genomic columns
@@ -59,7 +36,6 @@ subset_clinical_columns <- function(df){
     n <- n[ !grepl(p, n) ]
   }
   df <- df[, n]
-  write_to_s3integrated(df, name = "PER-PATIENT_nd_tumor_clinical.txt")
-  return(df)
+  df
 }
 
