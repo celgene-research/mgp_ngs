7676d9d35fe42636de12fd53ddbe5a5cf2875894
dkrozelle <dan.rozelle@gmail.com>
2017-02-22 11:18:25 -0800

    add filters to remove unsequenced patients before merge

--- Celgene/curation/table_merge.R
+++ Celgene/curation/table_merge.R
@@ -10,20 +10,23 @@ table_merge <- function(per.file, per.patient){
   #######################
   # CNV
   print("CNV Merge........................................", quote = F)
-  cnv <- GetS3Table(file.path(s3joint,"curated_CNV_ControlFreec.txt"))
-  df <-  merge_table_files(df1 = df, df2 = cnv, id = "File_Name")
+  new <- GetS3Table(file.path(s3joint,"curated_CNV_ControlFreec.txt"))
+  new <- filter(new, new$File_Name %in% per.file$File_Name)
+  df <-  merge_table_files(df, new, id = "File_Name")
   
   #######################
   # Biallelic Inactivation Flags
   print("BI Merge.........................................", quote = F)
-  bi <- GetS3Table(file.path(s3joint,"curated_BiallelicInactivation_Flag.txt"))
-  df <-  merge_table_files(df1 = df, df2 = bi, id = "File_Name")
+  new <- GetS3Table(file.path(s3joint,"curated_BiallelicInactivation_Flag.txt"))
+  new <- filter(new, new$File_Name %in% per.file$File_Name)
+  df <-  merge_table_files(df, new, id = "File_Name")
   
   #######################
   # SNV
   # print("SNV Merge........................................", quote = F)
-  # snv <- GetS3Table(file.path(s3joint,"curated_SNV_mutect2.txt"))
-  # df <-  merge_table_files(df1 = df, df2 = snv, id = "File_Name")
+  # new <- GetS3Table(file.path(s3joint,"curated_SNV_mutect2.txt"))
+  # new <- filter(new, new$File_Name %in% per.file$File_Name)
+  # df <-  merge_table_files(df, new, id = "File_Name")
   
   return(df)
   }
