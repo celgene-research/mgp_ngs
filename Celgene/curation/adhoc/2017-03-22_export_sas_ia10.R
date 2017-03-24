46f2dfd9eec75524d77200c68c7ac28ed0b4cd47
dkrozelle <dan.rozelle@gmail.com>
2017-03-24 10:11:55 -0700

    move to Rmd

--- Celgene/curation/adhoc/2017-03-22_export_sas_ia10.R
+++
@@ -1,7 +0,0 @@
-unified    <- GetS3Table(file.path(s3, "ClinicalData/ProcessedData/Integrated", "unified.clinical.nd.tumor.txt"), check.names = F)
-dict       <- GetS3Table(file.path(s3, "ClinicalData/ProcessedData/Integrated", "mgp_dictionary.xlsx"))
-export_to_sas(unified, dict)
-
-
-
-
