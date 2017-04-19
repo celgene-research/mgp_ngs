6acd5fd0f9357b4453703185b511ca0b63ff62b5
dkrozelle <dan.rozelle@gmail.com>
2017-04-19 08:02:46 -0700

    revise descriptive variables

--- Celgene/curation/2017-04-06_new_table_flow.R
+++ Celgene/curation/2017-04-06_new_table_flow.R
@@ -1,25 +1,47 @@
 source("curation_scripts.R")
-PRINTING = FALSE # turn off print to S3 when iterating
+PRINTING = TRUE # turn off print to S3 when iterating
 
+# remove "Study" from blood table
+# blood <- GetS3Table(file.path(s3, "ClinicalData/ProcessedData/JointData/curated_blood_2017-04-17.txt"), reader = "fread")
+# PutS3Table(object = blood[,Study:=NULL], s3.path = file.path(s3, "ClinicalData/ProcessedData/JointData/curated_blood_2017-04-17.txt"))
+
+
+### Filter JointData to Master
 system(paste('aws s3 cp',
-             file.path(s3, "ClinicalData/ProcessedData/Master/"),
+             file.path(s3, "ClinicalData/ProcessedData/JointData/"),
              local,
              '--recursive --exclude "*" --include "curated*"',
              '--exclude "archive*"', 
              sep = " "))
 
-# import master files
+# import Jointdata files
 files      <- list.files(local, full.names = T)
 dts        <- lapply(files, fread)
-names(dts) <- gsub("curated_(.*?)\\.txt", "\\1", tolower(basename(files)))
-# move metadata and clinical to front
-dts        <- c(dts['clinical'], dts[!grepl("clinical", names(dts))])
-dts        <- c(dts['metadata'], dts[!grepl("metadata", names(dts))])
-names(dts)
+names(dts) <- gsub("curated_(.*?)[_\\.].*txt", "\\1", tolower(basename(files)))
+# # move metadata and clinical to front
+# dts        <- c(dts['clinical'], dts[!grepl("clinical", names(dts))])
+# dts        <- c(dts['metadata'], dts[!grepl("metadata", names(dts))])
+# filter for only valid files and patients, put back filtered copies to MASTER
+valid.files <- dts$metadata[Excluded_Flag == 0 | is.na(Excluded_Flag) ,.(Patient, File_Name)]
 
+master <- lapply(names(dts), function(type){
+  dt <- dts[[type]]
+  if("File_Name" %in% names(dt)){dt <- dt[File_Name %in% valid.files$File_Name]
+  }else if("Patient" %in% names(dt)){dt <- dt[Patient %in% valid.files$Patient]
+  }else{stop("table doesn't have a filterable column")}
+  
+  n <- paste0("curated_", type, ".txt")
+  if(PRINTING) PutS3Table(dt, file.path(s3, "ClinicalData/ProcessedData/Master", n))
+  dt
+})
+names(master) <- names(dts)
+# count excluded rows removed 
+sapply(dts, dim) - sapply(master, dim)
+
+#######################################
 # join for per.file.all.txt
 per.file.all <- data.table()
-for( dt in dts ){
+for( dt in master ){
   if( nrow(per.file.all) == 0 ){
     per.file.all <- dt
   }else if( ("File_Name" %in% names(dt)) ){
@@ -31,38 +53,34 @@ if(PRINTING) PutS3Table(per.file.all, file.path(s3, "ClinicalData/ProcessedData/
 
 # filter EACH table for nd.tumor only
 ################################################
-nd.tumor.files    <- dts[['metadata']][Disease_Status == "ND" & Sample_Type_Flag == 1 & Disease_Type == "MM", File_Name]
-nd.tumor.patients <- dts[['metadata']][Disease_Status == "ND" & Sample_Type_Flag == 1 & Disease_Type == "MM", Patient]
-
-nd.tumor.dts <- lapply(names(dts), function(type){
-  dt <- dts[[type]]
+nd.tumor.files <- master$metadata[Disease_Status == "ND" & Sample_Type_Flag == 1 & Disease_Type == "MM" ,.(Patient, File_Name)]
+nd.tumor.dts <- lapply(names(master), function(type){
+  dt <- master[[type]]
   # if has a file_name use that, else use patient
   if( "File_Name" %in% names(dt) ){
     level <- "per.file"
-    dt    <- dt[File_Name %in% nd.tumor.files]
+    dt    <- dt[File_Name %in% nd.tumor.files$File_Name]
   }else{
     level <- "per.patient"
-    dt    <- dt[Patient %in% nd.tumor.patients]
+    dt    <- dt[Patient %in% nd.tumor.files$Patient]
   }
   n <- paste(level, type, "nd.tumor.txt", sep = ".")
   if(PRINTING) PutS3Table(dt, file.path(s3, "ClinicalData/ProcessedData/Integrated", n))
   dt
 })
-names(nd.tumor.dts) <- names(dts)
+names(nd.tumor.dts) <- names(master)
 # filter and print the ALL version too
-per.file.all.nd.tumor <- per.file.all[File_Name %in% nd.tumor.files]
+per.file.all.nd.tumor <- per.file.all[File_Name %in% nd.tumor.files$File_Name]
 if(PRINTING) PutS3Table(per.file.all.nd.tumor, 
                         file.path(s3, "ClinicalData/ProcessedData/Integrated", 
                                   "per.file.all.nd.tumor.txt"))
 
 # compare changes after removing relapse files/patients
-sapply(nd.tumor.dts, dim) - sapply(dts, dim)
-dim(per.file.all.nd.tumor) - dim(per.file.all)
-
+sapply(master, dim) - sapply(nd.tumor.dts, dim)
 
 
 # collapse each individual table to per.patient
-###########################################
+###############################################
 collapsed.dts <- lapply(nd.tumor.dts, function(dt){
   local_collapse_dt(dt, column.names = "Patient") })
 sapply(collapsed.dts, dim)
@@ -72,26 +90,34 @@ names(collapsed.dts) <- names(nd.tumor.dts)
 ###########################################
 null <- lapply(names(collapsed.dts), function(type){
   n <- paste("per.patient", type, "nd.tumor.txt", sep = ".")
-  PutS3Table(collapsed.dts[[type]],
-             file.path(s3, "ClinicalData/ProcessedData/Integrated", n))
+  if(PRINTING){PutS3Table(collapsed.dts[[type]],
+             file.path(s3, "ClinicalData/ProcessedData/Integrated", n))}
 })
 
 
 
 # cbind to get per.patient.unified.all.nd.tumor
 # TODO: retain File_Name for each data type for tracing
-collapsed.dts <- lapply(collapsed.dts, function(dt){setkey(dt, "Patient")})
-per.patient.unified.all.nd.tumor <- data.table()
+# 
+all <- data.table()
 for( dt in collapsed.dts ){
-  if( nrow(per.patient.unified.all.nd.tumor) == 0 ){
-    per.patient.unified.all.nd.tumor <- dt
+  setkey(dt, "Patient")
+  if( nrow(all) == 0 ){
+    all <- dt[,File_Name:=NULL]
   }else {
-    if( ("File_Name" %in% names(dt)) ) dt <- dt[,!"File_Name", with = F]
-    per.patient.unified.all.nd.tumor <- merge(per.patient.unified.all.nd.tumor, dt, all=TRUE, fill = T)
+    if( ("File_Name" %in% names(dt)) ) dt <- dt[,File_Name:=NULL]
+    all <- merge(all, dt, all=TRUE, fill = T)
   }
-  per.patient.unified.all.nd.tumor
+  print("Study" %in% names(all))
 }
-if(PRINTING) PutS3Table(per.patient.unified.all.nd.tumor, 
+
+# sort by dictionary
+dict      <- dict()
+matched   <- dict$names[dict$names %in% names(all)]
+unmatched <- names(all)[!names(all) %in% dict$names]
+
+setcolorder(all, c(matched, unmatched))
+if(PRINTING) PutS3Table(all, 
                         file.path(s3, "ClinicalData/ProcessedData/Integrated/per.patient.unified.all.nd.tumor.txt"))
 
 RPushbullet::pbPost("note", "done")
